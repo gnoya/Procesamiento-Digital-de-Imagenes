471e196dd4b2d4e9271933dfa81d2e6603673481
leoclb25 <35771607+leoclb25@users.noreply.github.com>
2018-02-06 20:15:36 +0100

    suvisado bien

--- Laboratorio 2/lab2p2.py
+++ Laboratorio 2/lab2p2.py
@@ -1,14 +1,21 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-image = cv2.imread('Test3.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+image = cv2.imread('Test3.jpg',)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 dimension=5
 kernel = np.ones((dimension,dimension),np.float32)/(dimension*dimension)
 dst = cv2.filter2D(image,-1,kernel)
+plt.figure()
 plt.subplot(121),plt.imshow(image),plt.title('Original')
 plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(dst),plt.title('Promedio')
 plt.xticks([]), plt.yticks([])
+
+
+plt.figure()
+perfilado=cv2.Laplacian(image,cv2.CV_64F)
+
+plt.imshow(perfilado)
 plt.show()
