f8179fb6dc69c29757e7052b918b2b17efca94c3
Noya <gabriel_noya@hotmail.com>
2018-02-06 19:31:19 -0400

    Equalizador optimizado

--- Laboratorio 2/lab22.py
+++ Laboratorio 2/lab22.py
@@ -2,22 +2,41 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image = cv2.imread('tornado.jpg')
+fileName = 'tornado.jpg'
+
+def hist(image):
+    return (cv2.calcHist([image], [0], None, [256], [0, 256]), 
+        cv2.calcHist([image], [1], None, [256], [0, 256]), cv2.calcHist([image], [2], None, [256], [0, 256]))
+
+def plotHistogram(b, g, r, text):
+    plt.figure()
+    plt.xlim([0, 256])
+    plt.plot(b, 'b')
+    plt.plot(g, 'g')
+    plt.plot(r, 'r')
+    plt.title(text)
+
+def plotBGRImage(image, text):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.figure()
+    plt.title(text)
+    plt.imshow(image)
+
+image = cv2.imread(fileName)
 
 b = cv2.equalizeHist(image[:,:,0])
 g = cv2.equalizeHist(image[:,:,1])
 r = cv2.equalizeHist(image[:,:,2])
 
-img = cv2.merge((b,g,r))
+eqImage = cv2.merge((b,g,r))
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+blueHistogram, greenHistogram, redHistogram  = hist(image)
+eqBlueHistogram, eqGreenHistogram, eqRedHistogram = hist(eqImage)
 
-plt.figure()
-plt.title('Imagen Original')
-plt.imshow(image)
-plt.figure()
-plt.title('Imagen con stretch')
-plt.imshow(img)
+plotHistogram(blueHistogram, greenHistogram, redHistogram, 'Histograma original')
+plotHistogram(eqBlueHistogram, eqGreenHistogram, eqRedHistogram, 'Histograma original')
+
+plotBGRImage(image, 'Imagen original')
+plotBGRImage(eqImage, 'Imagen equalizada')
 
 plt.show()
