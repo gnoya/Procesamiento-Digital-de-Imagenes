d30a027968b429b71dd66b61c7dc4642896f45c9
leoclb25 <35771607+leoclb25@users.noreply.github.com>
2018-02-10 13:06:30 +0100

    perfilado y gaussiano
    implementado el filtro gaussiano y el perfilado... falta la pregunta e y f

--- Laboratorio 2/lab2p2.py
+++ Laboratorio 2/lab2p2.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
+from scipy import ndimage 
 from matplotlib import pyplot as plt
 
-image = cv2.imread('Test3.jpg',)
+#suavisado a y b
+
+image = cv2.imread('NoyaCabrera.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 dimension=5
 kernel = np.ones((dimension,dimension),np.float32)/(dimension*dimension)
@@ -13,9 +16,26 @@ plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(dst),plt.title('Promedio')
 plt.xticks([]), plt.yticks([])
 
-
+#perfilado c
+imagen=cv2.imread('Pirata.tif')
+kernelp=np.ones((3,3),np.float32)*(-1)
+kernelp[1,1]=8
+imagenp=cv2.filter2D(imagen,-1,kernelp)
 plt.figure()
-perfilado=cv2.Laplacian(image,cv2.CV_64F)
+plt.subplot(131),plt.imshow(imagen),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(132),plt.imshow(imagenp),plt.title('Perfilado')
+plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(imagenp+imagen),plt.title('Imagen + perfilado')
+plt.xticks([]), plt.yticks([])
 
-plt.imshow(perfilado)
+
+#Filtro gaussiano d
+dim=5
+gaussian=cv2.GaussianBlur (image, (dim,dim), 0)
+plt.figure()
+plt.subplot(121),plt.imshow(image),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(gaussian),plt.title('Filtro gaussiano')
+plt.xticks([]), plt.yticks([])
 plt.show()
