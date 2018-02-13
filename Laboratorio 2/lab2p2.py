789d685211ff35e84d3355edb43bd949bfe0f4fd
Noya <gabriel_noya@hotmail.com>
2018-02-13 11:55:47 -0400

    parte e

--- Laboratorio 2/lab2p2.py
+++ Laboratorio 2/lab2p2.py
@@ -39,3 +39,20 @@ plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(gaussian),plt.title('Filtro gaussiano')
 plt.xticks([]), plt.yticks([])
 plt.show()
+
+#Parte e
+
+img = cv2.imread('XXX.XXX',0)
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=X)
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=X)
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplaciano'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.show()
+
