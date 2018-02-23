c82cd602f5cf47bee906a1db7c1b79269a4b5871
leoclb25 <35771607+leoclb25@users.noreply.github.com>
2018-02-22 21:38:41 -0400

    funciones

--- Tarea 1/tarea1.py
+++ Tarea 1/tarea1.py
@@ -1,32 +1,47 @@
 import cv2
 import numpy as np 
+import sys
 from matplotlib import pyplot as plt
+
+#Variables
+nombre="test1.png"
+kAlto=5
+nLetras=0
+
+def eliminarRuido():
+    #Eliminar ruido
+    kernel=np.ones((1,1),np.uint8)
+    img=cv2.dilate(img,kernel,iterations=1)
+    img=cv2.erode(img,kernel,iterations=1)
+
+def imagenBinaria():     
+    #Creacion imagen binaria
+    return cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,31,2)
+
+def elementoEstrural():
+    #Barrido con elemento estructurante
+    kernel=np.ones((kAlto,1),np.uint8)
+    img=cv2.dilate(img,kernel,iterations=1)
+    img=cv2.erode(img,kernel,iterations=1)
+    return img 
     
-Nletras=0
-img1=cv2.imread('test1.png')
-img=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+def buscarLetras():
+    #Buscar contornos
+    (_,contorno,_)=cv2.findContours(imgGray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for c in contorno:
+        (x,y,w,h)=cv2.boundingRect(c)
+        cv2.rectangle(imgOriginal,(x,y),(x+w,y+h),(0,255,0),1,cv2.LINE_AA)
+        nLetras+=1
+    
 
-#Eliminar ruido
-kernel=np.ones((1,1),np.uint8)
-img=cv2.dilate(img,kernel,iterations=1)
-img=cv2.erode(img,kernel,iterations=1)
+if __name__ == "__main__":
 
-#Creacion imagen binaria
-img= cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,31,2)
-
-#Barrido con elemento estructurante
-kernel=np.ones((5,1),np.uint8)
-img=cv2.dilate(img,kernel,iterations=1)
-img=cv2.erode(img,kernel,iterations=1)
-
-#buscar contornos
-(_,contorno,_)=cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-for c in contorno:
-    (x,y,w,h)=cv2.boundingRect(c)
-    cv2.rectangle(img1,(x,y),(x+w,y+h),(0,255,0),1,cv2.LINE_AA)
-    Nletras+=1
-
-plt.imshow(img1)
-print(Nletras)
-plt.show()
+    global imgOriginal=cv2.imread(nombre)
+    global img=cv2.cvtColor(imgOriginal,cv2.COLOR_BGR2GRAY)
+    eliminarRuido()
+    imagenBinaria()
+    elementoEstrural()
+    nLetras=buscarLetras()
+    plt.imshow()
+    plt.show()
+    print(nLetras)
