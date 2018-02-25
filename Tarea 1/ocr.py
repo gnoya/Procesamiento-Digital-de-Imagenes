aa6f347b66bf90d1127c97abe6b7166a9ae1f074
leoclb25 <35771607+leoclb25@users.noreply.github.com>
2018-02-24 20:15:54 -0400

    prueba ocr

--- Tarea 1/ocr.py
+++ Tarea 1/ocr.py
@@ -1,20 +1,24 @@
 import cv2
 import numpy as np 
 from matplotlib import pyplot as plt
-import pytesseract #https://github.com/nikhilkumarsingh/tesseract-python
-from PIL import Image 
 
+x=False
+if x:
+    import pytesseract #https://github.com/nikhilkumarsingh/tesseract-python
+    from PIL import Image 
+else:
+    print("noya pato")
 
-img=cv2.imread('test1.png')
-img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# img=cv2.imread('test4.tif')
+# img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-kernel = np.ones((1, 1), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
-img = cv2.erode(img, kernel, iterations=1)
+# kernel = np.ones((1, 1), np.uint8)
+# img = cv2.dilate(img, kernel, iterations=1)
+# img = cv2.erode(img, kernel, iterations=1)
 
-#Creacion imagen binaria
-img= cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,31,2)
+# #Creacion imagen binaria
+# img= cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,31,2)
 
-print(len(pytesseract.image_to_string(img)))
+# print(pytesseract.image_to_string(img))
 
 
--- Tarea 1/tarea1.py
+++ Tarea 1/tarea1.py
@@ -3,111 +3,68 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 
+showImage = True
+fileName = 'test1.png'
+kernelRows = 5
+mode=False
+#Chequeo de los argumentos de entrada
+x=1
+while(x<len(sys.argv)):
+    if sys.argv[x]=="-k":
+        x+=1
+        kernelRows=int(sys.argv[x])
+
+    if sys.argv[x]=="-n":
+        x+=1
+        fileName=sys.argv[x]
+    
+    if sys.argv[x]=="-ocr":
+        x+=1
+        mode=True
+    x+=1
+
+def noiseFilter(image, kernel):
+    # Eliminar ruido
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.erode(image, kernel, iterations=1)
+    return image
+
+def imageToBinary(image):     
+    # Creacion imagen binaria
+    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 2)
+
+def createKernel(rows, columns):
+    # Creacion del kernel
+    return np.ones((rows, columns), np.uint8)
+
+def applyStructuralElement(image, kernel):
+    # Barrido con elemento estructural
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.erode(image, kernel, iterations=1)
+    return image
+    
+def markCharacters(image, contour):
+    # Buscar contornos
+    for c in contour:
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+
 if __name__ == "__main__":
-    print("Cantidad de caracteres:", len(cv2.findContours(cv2.erode(cv2.dilate(cv2.adaptiveThreshold(cv2.erode(cv2.dilate(cv2.cvtColor(cv2.imread("test1.png"), cv2.COLOR_BGR2GRAY), np.ones((1, 1), np.uint8), iterations=1), np.ones((1, 1), np.uint8), iterations=1), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 2), np.ones((5, 1), np.uint8), iterations=1), np.ones((5, 1), np.uint8), iterations=1), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]))
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-# showImage = False
-
-# def noiseFilter(image, kernel):
-#     # Eliminar ruido
-#     image = cv2.dilate(image, kernel, iterations=1)
-#     image = cv2.erode(image, kernel, iterations=1)
-#     return image
-
-# def imageToBinary(image):     
-#     # Creacion imagen binaria
-#     return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 2)
-
-# def createKernel(rows, columns):
-#     # Creacion del kernel
-#     return np.ones((rows, columns), np.uint8)
-
-# def applyStructuralElement(image, kernel):
-#     # Barrido con elemento estructural
-#     image = cv2.dilate(image, kernel, iterations=1)
-#     image = cv2.erode(image, kernel, iterations=1)
-#     return image
-    
-# def markCharacters(image, contour):
-#     # Buscar contornos
-#     for c in contour:
-#         (x, y, w, h) = cv2.boundingRect(c)
-#         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
-
-# if __name__ == "__main__":
-#     origImage = cv2.imread(fileName)
-#     image = cv2.cvtColor(origImage, cv2.COLOR_BGR2GRAY)
-
-#     kernel = createKernel(1, 1)
-#     image = noiseFilter(image, kernel)
-#     image = imageToBinary(image)
-
-#     kernel = createKernel(kernelRows, 1)
-#     image = applyStructuralElement(image, kernel)
-
-#     (_,contour,_) = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     charNumber = len(contour)
-#     print("Cantidad de caracteres:", charNumber)
-
-#     if showImage:
-#         markCharacters(origImage, contour)
-#         plt.imshow(origImage)
-#         plt.show()
+    origImage = cv2.imread(fileName)
+    image = cv2.cvtColor(origImage, cv2.COLOR_BGR2GRAY)
+
+    kernel = createKernel(1, 1)
+    image = noiseFilter(image, kernel)
+    image = imageToBinary(image)
+
+    kernel = createKernel(kernelRows, 1)
+    image = applyStructuralElement(image, kernel)
+
+    (_,contour,_) = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    charNumber = len(contour)
+    print("Cantidad de caracteres:", charNumber)
+
+    if showImage:
+        markCharacters(origImage, contour)
+        plt.imshow(origImage)
+        plt.show()
